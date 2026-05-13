e300bf83c86b7cc9d0d04295898f720578fdb8e3
Otomezaa <virgowona@gmail.com>
2026-05-13 13:20:47 +0700

    menambah package model dan main dan mengisinya dengan class Layanan, Member, dan Main

---
+++ TUBES LANDRY/src/model/Layanan.java
@@ -0,0 +1,50 @@
+package model;
+
+public abstract class Layanan {
+
+    private int idLayanan;
+    private String namaLayanan;
+    private double hargaPerKg;
+    private int estimasiHari;
+
+    public Layanan(int idLayanan, String namaLayanan,
+            double hargaPerKg, int estimasiHari) {
+
+        if (idLayanan <= 0) {
+            idLayanan = 1;
+        }
+
+        if (namaLayanan == null || namaLayanan.isEmpty()) {
+            namaLayanan = "Tidak Diketahui";
+        }
+
+        if (hargaPerKg <= 0) {
+            hargaPerKg = 0;
+        }
+
+        if (estimasiHari < 0) {
+            estimasiHari = 0;
+        }
+
+        this.idLayanan = idLayanan;
+        this.namaLayanan = namaLayanan;
+        this.hargaPerKg = hargaPerKg;
+        this.estimasiHari = estimasiHari;
+    }
+
+    public double getHargaPerKg() {
+        return hargaPerKg;
+    }
+
+    public int getEstimasiHari() {
+        return estimasiHari;
+    }
+
+    public String getNamaLayanan() {
+        return namaLayanan;
+    }
+
+    public abstract double hitungBiaya(double berat);
+
+    public abstract String deskripsiLayanan();
+}
