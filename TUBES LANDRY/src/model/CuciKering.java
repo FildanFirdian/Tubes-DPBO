7abd28f27ef199ba4584b682d2ab2dad2037d347
Otomezaa <virgowona@gmail.com>
2026-05-13 13:48:59 +0700

    Masukkan CuciKering dan CuciSetrika

---
+++ TUBES LANDRY/src/model/CuciKering.java
@@ -0,0 +1,35 @@
+package model;
+
+public class CuciKering extends Layanan {
+
+    private String jenisProses;
+
+    public CuciKering(int idLayanan, String namaLayanan,
+            double hargaPerKg, int estimasiHari,
+            String jenisProses) {
+
+        super(idLayanan, namaLayanan, hargaPerKg, estimasiHari);
+
+        if (jenisProses == null || jenisProses.isEmpty()) {
+            jenisProses = "Reguler";
+        }
+
+        this.jenisProses = jenisProses;
+    }
+
+    @Override
+    public double hitungBiaya(double berat) {
+
+        if (berat <= 0) {
+            return 0;
+        }
+
+        return berat * getHargaPerKg();
+    }
+
+    @Override
+    public String deskripsiLayanan() {
+        return "Cuci Kering - " + jenisProses +
+                " (" + getEstimasiHari() + " Hari)";
+    }
+}
---
+++ TUBES LANDRY/src/model/CuciSetrika.java
@@ -0,0 +1,42 @@
+package model;
+
+public class CuciSetrika extends Layanan {
+
+    private String jenisProses;
+    private double biayaTambahan;
+
+    public CuciSetrika(int idLayanan, String namaLayanan,
+            double hargaPerKg, int estimasiHari,
+            String jenisProses,
+            double biayaTambahan) {
+
+        super(idLayanan, namaLayanan, hargaPerKg, estimasiHari);
+
+        if (jenisProses == null || jenisProses.isEmpty()) {
+            jenisProses = "Reguler";
+        }
+
+        if (biayaTambahan < 0) {
+            biayaTambahan = 0;
+        }
+
+        this.jenisProses = jenisProses;
+        this.biayaTambahan = biayaTambahan;
+    }
+
+    @Override
+    public double hitungBiaya(double berat) {
+
+        if (berat <= 0) {
+            return 0;
+        }
+
+        return (berat * getHargaPerKg()) + biayaTambahan;
+    }
+
+    @Override
+    public String deskripsiLayanan() {
+        return "Cuci Setrika - " + jenisProses +
+                " (" + getEstimasiHari() + " Hari)";
+    }
+}
